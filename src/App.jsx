af0e0cf55ad7c73a90021a63450a1c9e69ab4809
Pritha Rai <pritharai873@gmail.com>
2025-04-22 11:47:34 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Header from './components/Header'
-import About from './pages/About'
-import Projects from './pages/Projects'
-import Testimonials from './pages/Testimonials'
-import Contact from './components/Contact'
-import Layout from './components/Layout'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Testimonials from './pages/Testimonials';
+import Contact from './components/Contact';
+import Layout from './components/Layout';
 import ProjectDetail from './components/ProjectDetail';
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
@@ -31,12 +31,11 @@ const App = () => {
           <Route path="/projects" element={<Layout><Projects /></Layout>} />
           <Route path="/testimonials" element={<Layout><Testimonials /></Layout>} />
           <Route path="/project/:projectId" element={<ProjectDetail />} />
-          <Route path="/" element={<Projects />} />
-        <Route path="/project/:id" element={<ProjectDetail />} />
+          <Route path="*" element={<h1>404 - Page Not Found</h1>} />
         </Routes>
       </div>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
